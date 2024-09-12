d3d9fb8c0bf234188fb6ae9023efd684b8cde27f
Avinash Fernandes <avianthonf@pm.me>
2024-09-13 01:00:40 +0530

    Added platform specific build scripts

--- fromLinux-build-forWindows.sh
+++ fromLinux-build-forWindows.sh
@@ -4,4 +4,4 @@ x86_64-w64-mingw32-gcc -c modules/sqlite3.c
 x86_64-w64-mingw32-g++ -c main.cpp
 
 #Join them into one binary
-x86_64-w64-mingw32-g++ -o Banking-System-x86_64-Linux.bin main.o sqlite3.o
+x86_64-w64-mingw32-g++ -o Banking-System-x86_64-Windows.bin main.o sqlite3.o
