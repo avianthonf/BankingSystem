c2fbd2b264cdb047696363cf62eac92b43cc0799
Avinash Fernandes <avianthonf@pm.me>
2024-09-13 01:04:41 +0530

    Added platform specific build scripts

--- windows-build.cmd
+++ windows-build.cmd
@@ -3,4 +3,4 @@ gcc -c modules/sqlite3.c
 g++ -c main.cpp
 
 #Join them into one binary
-g++ -o Banking-System-x86_64-Windows.bin main.o sqlite3.o
+g++ -o Banking-System-x86_64-Windows.exe main.o sqlite3.o
