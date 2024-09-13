e1af3b4a4abd37af23139991c2f10470bd54b11e
Avinash Fernandes <118916911+avianthonf@users.noreply.github.com>
2024-09-13 13:55:54 +0530

    Delete fromLinux-build-forWindows.sh

--- fromLinux-build-forWindows.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Compile the parts
-x86_64-w64-mingw32-gcc -c modules/sqlite3.c
-x86_64-w64-mingw32-g++ -c main.cpp
-
-#Join them into one binary
-x86_64-w64-mingw32-g++ -o Banking-System-x86_64-Windows.exe main.o sqlite3.o
